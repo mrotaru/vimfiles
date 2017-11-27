4671129a810637120952405a6084cc4456e06b0a
Mihai Rotaru <mihai.rotaru@gmx.com>
2017-11-27 23:26:24 +0000

    simple script to install plugins via git clone

---
+++ install-plugins.bash
@@ -0,0 +1,3 @@
+git clone git@github.com:mhinz/vim-grepper.git ~/.vim/plugin/grepper
+git clone git@github.com:torbiak/probe.git ~/.vim/plugin/probe
+git clone git@github.com:w0rp/ale.git ~/.vim/pack/git-plugins/start/ale
